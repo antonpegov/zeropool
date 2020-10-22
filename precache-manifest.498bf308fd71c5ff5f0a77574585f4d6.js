07861160bacd9011bb61b4b37647b8c1c417da97
Anton Pegov <antonpegov@gmail.com>
2020-10-22 14:22:37 +0200

    Updates

---
+++ precache-manifest.498bf308fd71c5ff5f0a77574585f4d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc6dab2cd54004f5f473ea7172d4480",
+    "url": "/zeropool/index.html"
+  },
+  {
+    "revision": "e104114e2fdd339e36a8",
+    "url": "/zeropool/static/css/main.de5d707f.chunk.css"
+  },
+  {
+    "revision": "d44645ae0c8d8d525ab1",
+    "url": "/zeropool/static/js/2.368324be.chunk.js"
+  },
+  {
+    "revision": "397893adeca610c9fda4fe97323ae340",
+    "url": "/zeropool/static/js/2.368324be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e104114e2fdd339e36a8",
+    "url": "/zeropool/static/js/main.c8796bad.chunk.js"
+  },
+  {
+    "revision": "32fb697a0fa933644f6f",
+    "url": "/zeropool/static/js/runtime-main.e1971b33.js"
+  },
+  {
+    "revision": "0609f8d3285cad943f8341efecd9dfb4",
+    "url": "/zeropool/static/media/guy-fawkes-thanks.0609f8d3.png"
+  }
+]);
