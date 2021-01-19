8473b4f8c7c10462322962e3a3f56ca4eb4bbe32
Anton Pegov <antonpegov@gmail.com>
2021-01-19 14:21:02 +0100

    Add send view

--- src/wallet/components/Send/Send.tsx
+++ src/wallet/components/Send/Send.tsx
@@ -5,18 +5,38 @@ import './Send.scss';
 
 import { testIdBuilder } from 'shared/helpers/test/test-id-builder.helper';
 
+import { Wallet } from 'wallet/state/models/wallet';
+import { TextField } from '@material-ui/core';
+
 export const componentId = 'Send';
 
 const css = cn(componentId);
 const test = testIdBuilder(componentId);
 
-export interface SendProps { }
+export interface SendProps { 
+  wallet: Wallet,
+  onNextClick: (address: string, amount: number) => void,
+}
 
-export const Send: React.FC<SendProps> = () => {
+export const Send: React.FC<SendProps> = (props) => {
 
   return (
     <div className={css()} data-testid={test()}>
-      <h1> Send </h1>
+      <div className={css('Title')}> 
+        Send {props.wallet.address.symbol}
+      </div>
+
+      <form className={css('Inputs')} noValidate autoComplete="off">
+        <TextField className={css('AddressInput')} id="address" label="Address" />
+
+        <TextField className={css('AmountInput')} id="amount" label="Token amount" />
+      </form>
+
+      <div className={css('Next')}>
+        <button className={css('NextButton')} onClick={() => props.onNextClick}>
+          Next
+        </button>
+      </div>
     </div>
   )
 };      
