434efd49cfad6254d491f9f1f351dd760aff2408
Maksim Pegov <maksim.peg@gmail.com>
2021-11-17 18:06:20 +0300

    [ZP-UI-48] Send component unit test.

--- src/wallet/api/zeropool.api.ts
+++ src/wallet/api/zeropool.api.ts
@@ -148,7 +148,7 @@ const transfer = (account: number, to: string, amount: number, token: Token) => 
   )
 }
 
-const isPrivateAddress = (address: string, token: TokenSymbol) => {
+export const isPrivateAddress = (address: string, token: TokenSymbol) => {
   if (!hdWallet) {
     throw Error('API not available!')
   }
@@ -163,7 +163,7 @@ const convertValues = (coin: Coin) => (transactions: Transaction[]) =>
     timestamp: fixTimestamp(transaction.timestamp),
   }))
 
-const api = {
+export const api = {
   getAllBalances,
   getWalletBalance,
   getNetworkFee,
@@ -173,5 +173,3 @@ const api = {
   transfer,
   isPrivateAddress,
 }
-
-export default api
